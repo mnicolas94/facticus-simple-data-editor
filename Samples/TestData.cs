526f07e94b1cce633bab0f303c7fa4ffa4310c10
mnicolas94 <miguelcok27@gmail.com>
2024-02-08 08:02:13 -0500

    chore: add samples folder

---
+++ Samples/TestData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Samples
+{
+    [CreateAssetMenu(fileName = "TestData", menuName = "Tests/TestData", order = 0)]
+    public class TestData : ScriptableObject
+    {
+        [SerializeField] private string _stringField;
+        [SerializeField] private int _intField;
+    }
+}
---
+++ Samples/TestDataEditorWindow.cs
@@ -0,0 +1,17 @@
+﻿    using SimpleDataEditor.Editor;
+    using UnityEditor;
+    using UnityEngine;
+
+    namespace Samples
+    {
+        public class TestDataEditorWindow : DataTypeEditorWindow<TestData>
+        {
+            [MenuItem("Data/Test editor")]
+            private static void ShowWindow()
+            {
+                var window = GetWindow<TestDataEditorWindow>();
+                window.titleContent = new GUIContent("Test Data");
+                window.Show();
+            }
+        }
+    }
