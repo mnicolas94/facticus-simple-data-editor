9dd6334b60aa773ff23150d67b4c87dd7f22c5d9
mnicolas94 <miguelcok27@gmail.com>
2024-02-12 11:13:56 -0500

    fix: fix issue in code template

---
+++ Samples/Scripts/TestDataTwo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Samples
+{
+    [CreateAssetMenu(fileName = "TestData2", menuName = "Tests/TestData2", order = 0)]
+    public class TestDataTwo : ScriptableObject
+    {
+        [SerializeField] private List<int> _intList;
+    }
+}
